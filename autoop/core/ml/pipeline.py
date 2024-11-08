821cbf3222ae29e373b4e75954aa6b0c55208768
biancamalureanu <152000373+biancamalureanu@users.noreply.github.com>
2024-11-09 00:14:49 +0100

    Update pipeline.py

--- autoop/core/ml/pipeline.py
+++ autoop/core/ml/pipeline.py
@@ -33,8 +33,7 @@ class Pipeline:
         self._metrics = metrics
         self._artifacts = {}
         self._split = split
-        if (target_feature.type == "categorical" and
-                model.type != "classification"):
+        if target_feature.type == "categorical" and model.type != "classification":
             raise (ValueError
                    ("Model type must be classification "
                     "for categorical target feature"))
@@ -121,10 +120,8 @@ class Pipeline:
                          for vector in self._input_vectors]
         self._test_X = [vector[int(split * len(vector)):]
                         for vector in self._input_vectors]
-        self._train_y = self._output_vector[:int(split
-                                                 * len(self._output_vector))]
-        self._test_y = self._output_vector[int(split
-                                               * len(self._output_vector)):]
+        self._train_y = self._output_vector[:int(split * len(self._output_vector))]
+        self._test_y = self._output_vector[int(split * len(self._output_vector)):]
 
     def _compact_vectors(self, vectors: List[np.array]) -> np.array:
         """
