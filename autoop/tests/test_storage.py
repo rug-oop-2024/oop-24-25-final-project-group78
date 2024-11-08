ab586b316111bc19dd42b0166f3513aada262134
biancamalureanu <152000373+biancamalureanu@users.noreply.github.com>
2024-11-08 21:37:16 +0100

    Update test_storage.py

--- autoop/tests/test_storage.py
+++ autoop/tests/test_storage.py
@@ -1,10 +1,10 @@
-
 import unittest
 
 from autoop.core.storage import LocalStorage, NotFoundError
 import random
 import tempfile
 import os
+
 
 class TestStorage(unittest.TestCase):
 
@@ -42,10 +42,10 @@ class TestStorage(unittest.TestCase):
     def test_list(self):
         key = str(random.randint(0, 100))
         test_bytes = bytes([random.randint(0, 255) for _ in range(100)])
-        random_keys = [f"test{os.sep}{random.randint(0, 100)}" for _ in range(10)]
+        random_keys = [f"test{os.sep}{random.randint(0, 100)}"
+                       for _ in range(10)]
         for key in random_keys:
             self.storage.save(test_bytes, key)
         keys = self.storage.list("test")
         keys = [f"{os.sep}".join(key.split(f"{os.sep}")[-2:]) for key in keys]
         self.assertEqual(set(keys), set(random_keys))
-        
