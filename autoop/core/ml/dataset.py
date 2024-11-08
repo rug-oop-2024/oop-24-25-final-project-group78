a9ee032fd61cc8a573580d442d32b8dcf102fb5a
biancamalureanu <152000373+biancamalureanu@users.noreply.github.com>
2024-11-08 21:41:25 +0100

    Update dataset.py

--- autoop/core/ml/dataset.py
+++ autoop/core/ml/dataset.py
@@ -11,7 +11,10 @@ class Dataset(Artifact):
         super().__init__(type="dataset", *args, **kwargs)
 
     @staticmethod
-    def from_dataframe(data: pd.DataFrame, name: str, asset_path: str, version: str = "1.0.0", tags: List = [],
+    def from_dataframe(data: pd.DataFrame, name: str,
+                       asset_path: str,
+                       version: str = "1.0.0",
+                       tags: List = [],
                        metadata: Dict = {}):
         return Dataset(
             name=name,
@@ -22,16 +25,16 @@ class Dataset(Artifact):
             metadata=metadata
         )
     """
-    In the initialization of the object Dataset that it is returned in the static method, I added the arguments of the
+    In the initialization of the object Dataset that
+    it is returned in the static method, I added the arguments of the
     Artifact, that I previously mentioned.
     """
-        
+
     def read(self) -> pd.DataFrame:
         bytes = super().read()
         csv = bytes.decode()
         return pd.read_csv(io.StringIO(csv))
-    
+
     def save(self, data: pd.DataFrame) -> bytes:
         bytes = data.to_csv(index=False).encode()
         return super().save(bytes)
-    
