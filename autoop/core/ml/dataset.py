f927a3de8cd2630339c67e1721120eb900359af8
biancamalureanu <152000373+biancamalureanu@users.noreply.github.com>
2024-11-08 17:12:05 +0100

    Update dataset.py

--- autoop/core/ml/dataset.py
+++ autoop/core/ml/dataset.py
@@ -1,32 +1,37 @@
+from typing import List, Dict
+
 from autoop.core.ml.artifact import Artifact
-from abc import ABC, abstractmethod
 import pandas as pd
 import io
 
 
 class Dataset(Artifact):
-    """A class to represent an ML dataset"""
+
     def __init__(self, *args, **kwargs):
         super().__init__(type="dataset", *args, **kwargs)
 
     @staticmethod
-    def from_dataframe(data: pd.DataFrame, name: str,
-                       asset_path: str, version: str = "1.0.0"):
-        """ Create a dataset from a pandas dataframe."""
+    def from_dataframe(data: pd.DataFrame, name: str, asset_path: str, version: str = "1.0.0", tags: List = [],
+                       metadata: Dict = {}):
         return Dataset(
             name=name,
             asset_path=asset_path,
             data=data.to_csv(index=False).encode(),
             version=version,
+            tags=tags,
+            metadata=metadata
         )
-
+    """
+    In the initialization of the object Dataset that it is returned in the static method, I added the arguments of the
+    Artifact, that I previously mentioned.
+    """
+        
     def read(self) -> pd.DataFrame:
-        """ Read data from a given path """
         bytes = super().read()
         csv = bytes.decode()
         return pd.read_csv(io.StringIO(csv))
-
+    
     def save(self, data: pd.DataFrame) -> bytes:
-        """ Save data to a given path """
         bytes = data.to_csv(index=False).encode()
         return super().save(bytes)
+    
