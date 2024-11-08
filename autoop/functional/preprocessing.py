4d29a85388e2c34f5ad2586fd52b13c27eab04ef
biancamalureanu <152000373+biancamalureanu@users.noreply.github.com>
2024-11-08 19:41:22 +0100

    Update preprocessing.py

--- autoop/functional/preprocessing.py
+++ autoop/functional/preprocessing.py
@@ -24,7 +24,8 @@ def preprocess_features(features: List[Feature], dataset: Dataset) -> List[Tuple
         if feature.type == "numerical":
             scaler = StandardScaler()
             data = scaler.fit_transform(raw[feature.name].values.reshape(-1, 1))
-            artifact = {"type": "StandardScaler", "scaler": scaler.get_params()}
+            artifact = {"type": "StandardScaler",
+                        "scaler": scaler.get_params()}
             results.append((feature.name, data, artifact))
     # Sort for consistency
     results = list(sorted(results, key=lambda x: x[0]))
