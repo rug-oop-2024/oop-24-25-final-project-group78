615ffdac9229c3060314dc0e9d461930acc51d06
biancamalureanu <152000373+biancamalureanu@users.noreply.github.com>
2024-11-09 14:40:19 +0100

    Update feature.py

--- autoop/core/ml/feature.py
+++ autoop/core/ml/feature.py
@@ -1,21 +1,27 @@
 from enum import Enum
-
 from pydantic import BaseModel
 
 
 class FeatureType(str, Enum):
+    """
+    Enumeration representing the possible types of a feature:
+    numerical and categorical.
+    """
     NUMERICAL = "numerical"
     CATEGORICAL = "categorical"
 
 
-"""
-Enumeration that has two fields: numerical and categorical.
-"""
-
-
 class Feature(BaseModel):
+    """
+    Feature model representing a data feature with a name and type.
+    """
     name: str
     type: FeatureType
 
     def __str__(self) -> str:
+        """
+
+        Returns:
+            str: A string describing the feature with its name and type.
+        """
         return "Feature: " + self.name + " of type " + self.type
