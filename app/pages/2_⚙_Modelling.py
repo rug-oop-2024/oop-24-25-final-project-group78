7d4e6f3274e65509690b9a14a32678204d18370f
biancamalureanu <152000373+biancamalureanu@users.noreply.github.com>
2024-11-09 14:52:11 +0100

    Update 2_⚙_Modelling.py

--- app/pages/2_⚙_Modelling.py
+++ app/pages/2_⚙_Modelling.py
@@ -1,14 +1,20 @@
 import streamlit as st
-import pandas as pd
+# import pandas as pd
 
 from app.core.system import AutoMLSystem
-from autoop.core.ml.dataset import Dataset
+# from autoop.core.ml.dataset import Dataset
+# from autoop.functional.feature import detect_feature_types
 
 
+# Set the page configuration
 st.set_page_config(page_title="Modelling", page_icon="📈")
 
 
 def write_helper_text(text: str):
+    """Helper function to display helper text with custom styling.
+    Args:
+        text (str): Text to be displayed.
+    """
     st.write(f"<p style=\"color: #888;\">{text}</p>", unsafe_allow_html=True)
 
 
@@ -19,5 +25,3 @@ write_helper_text("In this section, you can design a machine "
 automl = AutoMLSystem.get_instance()
 
 datasets = automl.registry.list(type="dataset")
-
-# your code here
