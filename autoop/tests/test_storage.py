c738bd974bdb8b235eee8fe4c49df5aa125451e5
biancamalureanu <152000373+biancamalureanu@users.noreply.github.com>
2024-11-09 14:35:42 +0100

    Update test_storage.py

--- autoop/tests/test_storage.py
+++ autoop/tests/test_storage.py
@@ -1,5 +1,4 @@
 import unittest
-
 from autoop.core.storage import LocalStorage, NotFoundError
 import random
 import tempfile
@@ -7,45 +6,69 @@ import os
 
 
 class TestStorage(unittest.TestCase):
+    """
+    Unit tests for the LocalStorage class
+    """
 
     def setUp(self):
+        """
+        Set up the test environment by creating a temporary directory
+        for storage and initializing a LocalStorage instance.
+        """
         temp_dir = tempfile.mkdtemp()
         self.storage = LocalStorage(temp_dir)
 
     def test_init(self):
+        """
+        Test the initialization of the LocalStorage instance
+        """
         self.assertIsInstance(self.storage, LocalStorage)
 
     def test_store(self):
-        key = str(random.randint(0, 100))
+        """
+        Test the 'save' and 'load' functionality
+        by saving a byte sequence to a specific
+        key and verifying that it can be retrieved correctly.
+        Also tests the behavior when
+        trying to load a non-existent key, expecting a NotFoundError.
+        """
+        # key = str(random.randint(0, 100))
         test_bytes = bytes([random.randint(0, 255) for _ in range(100)])
         key = f"test{os.sep}path"
         self.storage.save(test_bytes, key)
         self.assertEqual(self.storage.load(key), test_bytes)
+
         otherkey = f"test{os.sep}otherpath"
-        # should not be the same
-        try:
+        with self.assertRaises(NotFoundError):
             self.storage.load(otherkey)
-        except Exception as e:
-            self.assertIsInstance(e, NotFoundError)
 
     def test_delete(self):
-        key = str(random.randint(0, 100))
+        """
+        Test the 'delete' functionality
+        """
+        # key = str(random.randint(0, 100))
         test_bytes = bytes([random.randint(0, 255) for _ in range(100)])
         key = f"test{os.sep}path"
         self.storage.save(test_bytes, key)
         self.storage.delete(key)
-        try:
-            self.assertIsNone(self.storage.load(key))
-        except Exception as e:
-            self.assertIsInstance(e, NotFoundError)
+        with self.assertRaises(NotFoundError):
+            self.storage.load(key)
 
     def test_list(self):
-        key = str(random.randint(0, 100))
+        """
+        Test the 'list' functionality
+        """
+        # key = str(random.randint(0, 100))
         test_bytes = bytes([random.randint(0, 255) for _ in range(100)])
-        random_keys = [f"test{os.sep}{random.randint(0, 100)}"
-                       for _ in range(10)]
+        random_keys = [(f"test{os.sep}"
+                        f"{random.randint(0, 100)}") for _ in range(10)]
         for key in random_keys:
             self.storage.save(test_bytes, key)
+
         keys = self.storage.list("test")
         keys = [f"{os.sep}".join(key.split(f"{os.sep}")[-2:]) for key in keys]
         self.assertEqual(set(keys), set(random_keys))
+
+
+if __name__ == "__main__":
+    unittest.main()
