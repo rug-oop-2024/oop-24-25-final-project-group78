38bc5582c1ca1fc17bd539d2eb2de9ca5aa8dbfd
biancamalureanu <152000373+biancamalureanu@users.noreply.github.com>
2024-11-09 14:42:47 +0100

    Update __init__.py

--- autoop/core/ml/model/__init__.py
+++ autoop/core/ml/model/__init__.py
@@ -1,14 +1,40 @@
+from typing import Dict
 
 from autoop.core.ml.model.model import Model
-from autoop.core.ml.model.regression import MultipleLinearRegression
+
+from autoop.core.ml.model.regression.multiple_linear_regression import MultipleLinearRegression, Lasso, Ridge
+from autoop.core.ml.model.classification.classification import KNN, LogisticRegressionModel, SVCModel
 
 REGRESSION_MODELS = [
-]  # add your models as str here
+    "MultipleLinearRegression",
+    "Lasso",
+    "Ridge"
+]
 
 CLASSIFICATION_MODELS = [
-]  # add your models as str here
+    "KNN",
+    "LogisticRegressionModel",
+    "SVCModel"
+]
 
 
-def get_model(model_name: str) -> Model:
+def get_model(model_name: str, params: Dict = None) -> Model:
     """Factory function to get a model by name."""
-    raise NotImplementedError("To be implemented.")
+    if model_name in REGRESSION_MODELS:
+        if model_name == "MultipleLinearRegression":
+            return MultipleLinearRegression(params=params)
+        elif model_name == "Lasso":
+            return Lasso(params=params)
+        elif model_name == "Ridge":
+            return Ridge(params=params)
+    elif model_name in CLASSIFICATION_MODELS:
+        if model_name == "KNN":
+            return KNN(params=params)
+        elif model_name == "LogisticRegressionModel":
+            return LogisticRegressionModel(params=params)
+        elif model_name == "SVCModel":
+            return SVCModel(params=params)
+    else:
+        raise ValueError(
+            f"Model '{model_name}' is not recognized. "
+            f"Available models: {REGRESSION_MODELS + CLASSIFICATION_MODELS}")
