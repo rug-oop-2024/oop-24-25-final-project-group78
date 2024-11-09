872328ab83476d97bac6d95f0335987db6fb2e04
biancamalureanu <152000373+biancamalureanu@users.noreply.github.com>
2024-11-09 14:33:59 +0100

    Update test_database.py

--- autoop/tests/test_database.py
+++ autoop/tests/test_database.py
@@ -1,5 +1,4 @@
 import unittest
-
 from autoop.core.database import Database
 from autoop.core.storage import LocalStorage
 import random
@@ -7,21 +6,36 @@ import tempfile
 
 
 class TestDatabase(unittest.TestCase):
+    """
+    Unit tests for the Database class
+    """
 
     def setUp(self):
+        """
+        Set up the test environment
+        """
         self.storage = LocalStorage(tempfile.mkdtemp())
         self.db = Database(self.storage)
 
     def test_init(self):
+        """
+        Test the initialization of the Database instance
+        """
         self.assertIsInstance(self.db, Database)
 
     def test_set(self):
+        """
+        Test the 'set' functionality of the Database
+        """
         id = str(random.randint(0, 100))
         entry = {"key": random.randint(0, 100)}
         self.db.set("collection", id, entry)
         self.assertEqual(self.db.get("collection", id)["key"], entry["key"])
 
     def test_delete(self):
+        """
+        Test the 'delete' functionality of the Database
+        """
         id = str(random.randint(0, 100))
         value = {"key": random.randint(0, 100)}
         self.db.set("collection", id, value)
@@ -30,7 +44,10 @@ class TestDatabase(unittest.TestCase):
         self.db.refresh()
         self.assertIsNone(self.db.get("collection", id))
 
-    def test_persistance(self):
+    def test_persistence(self):
+        """
+        Test the persistence of data in the Database
+        """
         id = str(random.randint(0, 100))
         value = {"key": random.randint(0, 100)}
         self.db.set("collection", id, value)
@@ -38,6 +55,9 @@ class TestDatabase(unittest.TestCase):
         self.assertEqual(other_db.get("collection", id)["key"], value["key"])
 
     def test_refresh(self):
+        """
+        Test the 'refresh' functionality
+        """
         key = str(random.randint(0, 100))
         value = {"key": random.randint(0, 100)}
         other_db = Database(self.storage)
@@ -46,8 +66,15 @@ class TestDatabase(unittest.TestCase):
         self.assertEqual(other_db.get("collection", key)["key"], value["key"])
 
     def test_list(self):
+        """
+        Test the 'list' functionality
+        """
         key = str(random.randint(0, 100))
         value = {"key": random.randint(0, 100)}
         self.db.set("collection", key, value)
-        # collection should now contain the key
+        # Check that the collection contains the key-value pair
         self.assertIn((key, value), self.db.list("collection"))
+
+
+if __name__ == "__main__":
+    unittest.main()
