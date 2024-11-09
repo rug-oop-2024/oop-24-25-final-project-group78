ecf3369a786c4edb7861405b162099a073ee5e80
biancamalureanu <152000373+biancamalureanu@users.noreply.github.com>
2024-11-09 14:35:08 +0100

    Update test_pipeline.py

--- autoop/tests/test_pipeline.py
+++ autoop/tests/test_pipeline.py
@@ -11,8 +11,14 @@ from autoop.core.ml.metric import MeanSquaredError
 
 
 class TestPipeline(unittest.TestCase):
+    """
+    Unit tests for the Pipeline class
+    """
 
     def setUp(self) -> None:
+        """
+        Set up the test environment
+        """
         data = fetch_openml(name="adult", version=1, parser="auto")
         df = pd.DataFrame(
             data.data,
@@ -36,13 +42,22 @@ class TestPipeline(unittest.TestCase):
         self.ds_size = data.data.shape[0]
 
     def test_init(self):
+        """
+        Test the initialization of the Pipeline instance
+        """
         self.assertIsInstance(self.pipeline, Pipeline)
 
     def test_preprocess_features(self):
+        """
+        Test the feature preprocessing step
+        """
         self.pipeline._preprocess_features()
         self.assertEqual(len(self.pipeline._artifacts), len(self.features))
 
     def test_split_data(self):
+        """
+        Test the data splitting functionality
+        """
         self.pipeline._preprocess_features()
         self.pipeline._split_data()
         self.assertEqual(self.pipeline._train_X[0].shape[0],
@@ -51,15 +66,18 @@ class TestPipeline(unittest.TestCase):
                          self.ds_size - int(0.8 * self.ds_size))
 
     def test_train(self):
+        """
+        Test the training process of the model within the Pipeline
+        """
         self.pipeline._preprocess_features()
         self.pipeline._split_data()
         self.pipeline._train()
         self.assertIsNotNone(self.pipeline._model.params)
-        '''
-        I modified "parameters" in "params" based on my implementation
-        '''
 
     def test_evaluate(self):
+        """
+        Test the evaluation process of the Pipeline
+        """
         self.pipeline._preprocess_features()
         self.pipeline._split_data()
         self.pipeline._train()
@@ -67,3 +85,7 @@ class TestPipeline(unittest.TestCase):
         self.assertIsNotNone(self.pipeline._predictions)
         self.assertIsNotNone(self.pipeline._metrics_results_test)
         self.assertEqual(len(self.pipeline._metrics_results_test), 1)
+
+
+if __name__ == "__main__":
+    unittest.main()
