308082ed7dfbe5f5519ce537e014ea0b7ec11f6d
biancamalureanu <152000373+biancamalureanu@users.noreply.github.com>
2025-01-25 17:48:49 +0100

    Update 1_📊_Datasets.py

--- app/pages/1_📊_Datasets.py
+++ app/pages/1_📊_Datasets.py
@@ -4,17 +4,15 @@ import pandas as pd
 from app.core.system import AutoMLSystem
 from autoop.core.ml.dataset import Dataset
 
-# import sys
-# import os
 
 # sys.path.insert(0,
 #                 os.path.abspath
-#                 ("C:/Users/Bianca/Desktop/OOP 2/"
-#                  "oop-24-25-final-project-group_80"))
-"""
-I used this because my system was not
-using the right path to the app.
-"""
+#                 ("C:/Users/Bianca/Desktop/"
+#                  "oop-24-25-final-project-group_78"))
+# """
+# I used this because my system was not
+# using the right path to the app.
+# """
 
 
 st.set_page_config(page_title="Manage Datasets", page_icon="📊")
@@ -33,15 +31,18 @@ def app() -> None:
     uploaded_file = st.file_uploader('Choose a CSV file', type='csv')
 
     if uploaded_file is not None:
-        df = pd.read_csv(uploaded_file)
-        st.write("Preview of the uploaded dataset: " + uploaded_file.name)
-        st.write(df.head())
+        try:
+            df = pd.read_csv(uploaded_file)
+            st.write("Preview of the uploaded dataset: " + uploaded_file.name)
+            st.write(df.head())
 
-        dataset = Dataset.from_dataframe(df,
-                                         name=uploaded_file.name,
-                                         asset_path=uploaded_file.name)
+            dataset = Dataset.from_dataframe(df,
+                                             name=uploaded_file.name,
+                                             asset_path=uploaded_file.name)
 
-        AutoMLSystem.get_instance().registry.register(dataset)
+            AutoMLSystem.get_instance().registry.register(dataset)
+        except pd.errors.EmptyDataError:
+            st.write("Nice try, but the dataset can't be empty :)!")
 
 
 app()
